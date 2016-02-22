aa6bced199f2184e6d754f403b9ed26c47e43508
Mike Silvis <mikesilvis@gmail.com>
2016-02-21 16:37:58 -0800

    building out the flash message and ability to select a future date

--- app/controllers/plan_controller.rb
+++ app/controllers/plan_controller.rb
@@ -14,8 +14,8 @@ class PlanController < ApplicationController
   helper_method :travel_times
 
   def departure_times
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map do |future_times|
-      (DateTime.now + future_times.hours)
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map do |future_hour|
+      (leave_date + future_hour.hours)
     end
   end
   helper_method :departure_times
@@ -24,5 +24,18 @@ class PlanController < ApplicationController
     if params[:location]
       Google::Maps.route(params[:location][:start], params[:location][:end], departure_time: time.strftime('%s'), key: API_KEY).duration_in_traffic.text
     end
+  end
+
+  def leave_date
+    if params[:location][:departure].present?
+      leave_time = DateTime.parse("#{params[:location][:departure]} #{ActiveSupport::TimeZone.new('America/Los_Angeles')}")
+      if leave_time.future?
+        return leave_time
+      else
+        flash[:notice] = "Departure time must be in the future"
+      end
+    end
+
+    DateTime.now
   end
 end
